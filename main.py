c90fe2864d0f6f8134309b6e58eaa9172acd6bdd
Jasper Pult <jasper.pult@lht.dlh.de>
2022-12-08 15:39:33 +0100

    Add shoot function

--- main.py
+++ main.py
@@ -4,29 +4,31 @@ from pybricks.parameters import Button, Color, Direction, Port, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
-# Say hello :)
 print("Hello, AVIATIER!")
-
 
 class Aviatier():
     ROCK_COLORS = Color.RED
 
     def __init__(self):
         # Wheels
-        self.rightMotor = Motor(Port.A)
-        self.leftMotor = Motor(Port.B)
+        #self.rightMotor = Motor(Port.A)
+        self.gun = Motor(Port.B)
         # Grabber
-        self.grabber = Motor(Port.F)
+        #self.grabber = Motor(Port.F)
         # Sensors
         # self.sherloc = ColorSensor(Port.C)
-        self.eyes = UltrasonicSensor(Port.D)
+        # self.eyes = UltrasonicSensor(Port.D)
 
         self.hub = InventorHub()
-        self.scan_distance()
+        self.shoot()
 
     def stop(self):
         self.rightMotor.stop()
         self.leftMotor.stop()
+
+    def shoot(self):
+        self.gun.run_angle(100, 90)
+      
 
     def scan_distance(self):
         while True:
@@ -42,48 +44,48 @@ class Aviatier():
                 wait(100)
 
 
-def find_ball(self):
-    print("scanning for colour RED")
-    while True:
-        # If a ball color is detected, log it
-        if self.sherloc.color() is self.ROCK_COLORS:
-            print("Colour found!")
-            self.hub.speaker.beep()
+    def find_ball(self):
+        print("scanning for colour RED")
+        while True:
+            # If a ball color is detected, log it
+            if self.sherloc.color() is self.ROCK_COLORS:
+                print("Colour found!")
+                self.hub.speaker.beep()
 
-        wait(100)
-
-
-def grab():
-    while True:
-        grabSpeed = 100
-        grabber.run_target(-1 * grabSpeed)
-        wait()
-        grabber.stop()
-        wait(1000)
+            wait(100)
 
 
-def drive():
-    while True:
-        # Set the speed based on dial angle
-        speed = 500
-        if abs(speed) < 100:
-            speed = 0
+    def grab():
+        while True:
+            grabSpeed = 100
+            grabber.run_target(-1 * grabSpeed)
+            wait()
+            grabber.stop()
+            wait(1000)
 
-        leftMotor.run(speed)
-        rightMotor.run(-1 * speed)
 
-        wait(1000)
-        leftMotor.stop()
-        rightMotor.stop()
-        wait(1000)
+    def drive():
+        while True:
+            # Set the speed based on dial angle
+            speed = 500
+            if abs(speed) < 100:
+                speed = 0
 
-        print(leftMotor.angle())
-        print("+90=")
-        newAngle = leftMotor.angle() + 90
+            leftMotor.run(speed)
+            rightMotor.run(-1 * speed)
 
-        print(newAngle)
+            wait(1000)
+            leftMotor.stop()
+            rightMotor.stop()
+            wait(1000)
 
-        leftMotor.run_angle(500, newAngle)
+            print(leftMotor.angle())
+            print("+90=")
+            newAngle = leftMotor.angle() + 90
+
+            print(newAngle)
+
+            leftMotor.run_angle(500, newAngle)
 
 
 aviatier = Aviatier()
