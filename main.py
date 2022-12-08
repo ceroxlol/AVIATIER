d6d5c9704ad32258f5a3db10d8ded32fc6abb757
Christopher Werner <christopher.werner@netlight.com>
2022-12-08 16:41:29 +0100

    Implement all the faces.

--- main.py
+++ main.py
@@ -1,50 +1,207 @@
+from pybricks.geometry import Matrix
 from pybricks.hubs import InventorHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
-from pybricks.parameters import Button, Color, Direction, Port, Stop
-from pybricks.robotics import DriveBase
+from pybricks.parameters import Color, Side, Icon
 from pybricks.tools import wait, StopWatch
 
-# We'll use two motors. One is a dial
-# to set the speed of the other motor.
-leftMotor = Motor(Port.B)
-rightMotor = Motor(Port.A)
-grabber = Motor(Port.F)
 
-# Say hello :)
-print("Hello, Pybricks!")
+class Aviatier:
+    def __init__(self):
+        self.hub = InventorHub()
+        self.hub.display.orientation(up=Side.RIGHT)
+        self.display = self.hub.display
+        self.light = self.hub.light
 
-# First, we'll move the dial to zero.
-# dial.run_target(500, 0, Stop.COAST)
+    def angry_face(self):
+        self.light.on(Color.RED)
 
-while True:
-    grabSpeed = 100
-    grabber.run_target(-1*grabSpeed)
-    wait()
-    grabber.stop()
-    wait(1000)
+        sadface = Matrix(
+            [
+                [100, 0, 0, 0, 100],
+                [0, 100, 0, 100, 0],
+                [0, 0, 0, 0, 0],
+                [25, 100, 100, 100, 25],
+                [100, 15, 0, 15, 100]
+            ]
+        )
+
+        self.display.icon(sadface)
+
+        wait(5000)
+
+    def happy_face(self):
+        self.light.on(Color.RED)
+
+        happyface = Matrix(
+            [
+                [15, 100, 0, 100, 15],
+                [100, 30, 100, 30, 100],
+                [0, 0, 0, 0, 0],
+                [100, 25, 10, 25, 100],
+                [15, 100, 100, 100, 15]
+            ]
+        )
+
+        self.display.icon(happyface)
+
+        wait(5000)
+
+    def happy_face2(self):
+        self.light.on(Color.RED)
+
+        happyface = Matrix(
+            [
+                [0, 100, 0, 100, 0],
+                [0, 100, 0, 100, 0],
+                [100, 0, 0, 0, 100],
+                [15, 100, 0, 100, 15],
+                [0, 15, 100, 15, 0]
+            ]
+        )
+
+        self.display.icon(happyface)
+
+        wait(5000)
+
+    def pissed_face(self):
+        self.light.on(Color.RED)
+
+        pissedface = Matrix(
+            [
+                [0, 0, 0, 0, 0],
+                [100, 100, 0, 100, 100],
+                [0, 0, 0, 0, 0],
+                [25, 100, 100, 100, 25],
+                [100, 15, 0, 15, 100]
+            ]
+        )
+
+        self.display.icon(pissedface)
+
+        wait(5000)
+
+    def blinking_sad_face(self):
+        self.light.on(Color.RED)
+
+        matrix = [Matrix(
+            [
+                [100, 100, 0, 100, 100],
+                [100, 100, 0, 100, 100],
+                [0, 0, 0, 0, 0],
+                [25, 100, 100, 100, 25],
+                [100, 15, 0, 15, 100]
+            ]
+        ), Matrix(
+            [
+                [0, 0, 0, 0, 0],
+                [100, 100, 0, 100, 100],
+                [0, 0, 0, 0, 0],
+                [25, 100, 100, 100, 25],
+                [100, 15, 0, 15, 100]
+            ]
+        )]
+
+        self.display.animate(matrix, 600)
+        wait(10000)
+
+    def heart_and_stars_face(self):
+        heart = Matrix(
+            [
+                [10, 100, 25, 100, 10],
+                [100, 10, 100, 10, 100],
+                [100, 10, 0, 10, 100],
+                [0, 100, 0, 100, 0],
+                [0, 0, 100, 0, 0]
+            ]
+        )
+
+        heart_icon = Icon.HEART
+
+        star = Matrix(
+            [
+                [50, 0, 100, 0, 50],
+                [0, 50, 100, 50, 0],
+                [100, 100, 100, 100, 100],
+                [0, 50, 100, 50, 0],
+                [50, 0, 100, 0, 50]
+            ]
+        )
+        self.display.animate([heart_icon, star], 10)
+
+    def battery_face(self):
+        self.light.on(Color.RED)
+
+        battery1 = Matrix(
+            [
+                [0, 0, 0, 0, 0],
+                [100, 100, 100, 100, 100],
+                [100, 0, 0, 100, 100],
+                [100, 100, 100, 100, 100],
+                [0, 0, 0, 0, 0]
+            ]
+        )
+        battery2 = Matrix(
+            [
+                [0, 0, 0, 0, 0],
+                [100, 100, 100, 100, 100],
+                [100, 0, 0, 0, 100],
+                [100, 100, 100, 100, 100],
+                [0, 0, 0, 0, 0]
+            ]
+        )
+        self.display.animate([battery1, battery2], 50)
+        wait(5000)
 
 
-def drive():
-    while True:
-        # Set the speed based on dial angle
-        speed = 500
-        if abs(speed) < 100:
-            speed = 0
+    def christmas_face(self):
+        self.light.animate([Color.GREEN, Color.RED], 500)
 
-        leftMotor.run(speed)
-        rightMotor.run(-1*speed)
+        christmas = Matrix(
+            [
+                [0, 0, 100, 0, 0],
+                [0, 25, 100, 25, 0],
+                [25, 50, 100, 50, 25],
+                [50, 75, 100, 75, 50],
+                [75, 100, 100, 100, 75]
+            ]
+        )
+        self.display.icon(christmas)
+        wait(5000)
 
-        wait(1000)
-        leftMotor.stop()
-        rightMotor.stop()
-        wait(1000)
+    def plane_face(self):
+        self.light.animate([Color.GREEN, Color.BLUE, Color.VIOLET], 500)
 
-        print(leftMotor.angle())
-        print("+90=")
-        newAngle = leftMotor.angle()+90
+        plane = Matrix(
+            [
+                [100, 25, 0, 0, 0],
+                [25, 100, 50, 10, 0],
+                [0, 50, 100, 75, 75],
+                [0, 10, 75, 100, 15],
+                [0, 0, 75, 15, 100]
+            ]
+        )
+        self.display.icon(plane)
+        wait(5000)
 
-        print(newAngle)
+    def aviatar_face(self):
+        self.light.animate([Color.GREEN, Color.BLUE, Color.VIOLET], 500)
 
-        leftMotor.run_angle(500, newAngle)
+        aviatar = Matrix(
+            [
+                [50, 100, 100, 100, 100],
+                [100, 50, 0, 0, 100],
+                [100, 100, 100, 0, 100],
+                [0, 0, 100, 50, 100],
+                [0, 0, 100, 100, 50]
+            ]
+        )
 
-        wait(1000)
+        self.display.icon(aviatar)
+        wait(5000)
+
+    def explosion(self):
+        self.light.on(Color.RED)
+
+        # o O --> Ringe
+
+
+aviatier = Aviatier()
